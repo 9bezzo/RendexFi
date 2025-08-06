ba0b93ff04ba41ad0a597bff6928a7a942cc893c
bezzo.rx <bezzo19@gmx.de>
2025-08-06 20:18:57 +0200

    App.tsx aktualisieren

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-import React from "react";
-import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
-import { WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
-
-require('@solana/wallet-adapter-react-ui/styles.css');
-
-const endpoint = "https://api.devnet.solana.com";
+import React from 'react';
+import WalletConnects from './compontents/WalletConnect';
 
 export default function App() {
-  const wallets = [
-    new PhantomWalletAdapter(),
-  ];
-
   return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 50 }}>
-            <h1>Solana Dapp Beispiel</h1>
-            <WalletMultiButton />
-            <p>Verbinde deine Wallet, um loszulegen!</p>
-          </div>
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItmes: 'center', marginTop: 50 }}>
+      <WalletConnects />
+    </div>
   );
 }
